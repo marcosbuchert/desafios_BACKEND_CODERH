06c1760b26d8ea1e7f7f066d1b896b4f0fa5c25e
Marcos A. Büchert <49037687+marcosbuchert@users.noreply.github.com>
2022-03-22 19:15:25 -0300

    Create clases.js

---
+++ clases.js
@@ -0,0 +1,12 @@
+class Personaje{
+    constructor(nombre, salud, fuerza, defenza){
+        this.nombre = nombre
+        this.salud = salud
+        this.fuerza = fuerza
+        this.defenza = defenza
+    }
+}
+
+let prota = new Personaje(Mocte, 100, 200, 50)
+
+console.log(prota)
